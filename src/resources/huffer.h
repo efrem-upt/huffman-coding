23723162fb527bd8fec12324bdb64c8ce8bd33f6
Dragoș Efrem <101568589+dragosefrem@users.noreply.github.com>
2022-04-09 16:35:50 +0300

    modified huffer.h: added note and key structure plus methods and variables to be used in the coding process

---
+++ src/resources/huffer.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct KeyType {
+    char key;
+    int probability;
+    int isSpecialNode;
+}KeyType;
+
+typedef struct Node {
+    // node in Huffman/Binary tree
+    KeyType key;
+    struct Node* leftChild;
+    struct Node* rightChild;
+}
+
+KeyType keys[256];
+Node* HuffmanRoot; // root of the Huffman tree
+Node* KeysRoot; // root of the binary tree containing Huffman trees
+void addKeyToBinaryTree(char key) // adds the specified key in the binary tree containing all the keys
+void removeKeyFromBinaryTree(char key) // removes the specified key in the binary tree containing all the keys
+Node* parseKeysTree(KeyType keys[]); // parses the keys in the binary tree and returns the corresponding Huffman Tree
+void parseKeysFile(FILE* file); // parses the file and creates the binary tree containing the keys
